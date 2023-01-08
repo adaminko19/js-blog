235ccaa85f6f5db49c8db8c094bee8e5bb208fbf
Adam Błaszczyk <adaminko12@gmail.com>
2023-01-08 17:27:28 +0100

    add class active to article

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
 /* [DONE] remove class 'active' from all article links */
@@ -9,7 +10,7 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-/* [IN PROGRESS] add class 'active' to the clicked link */
+/* [DONE] add class 'active' to the clicked link */
 console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
 /* [DONE] remove class 'active' from all articles */
@@ -19,9 +20,13 @@ for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
 
-/* get 'href' attribute from the clicked link */
-/* find the correct article using the selector ( value of 'href' attribute) */
-/* add class 'active' to the correct article */
+/* [DONE] get 'href' attribute from the clicked link */
+const articleSelector = clickedElement.getAttribute('href');
+
+/* [DONE] find the correct article using the selector ( value of 'href' attribute) */
+const targetArticle = document.querySelector(articleSelector);
+/* [IN PROGRESS] add class 'active' to the correct article */
+targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
