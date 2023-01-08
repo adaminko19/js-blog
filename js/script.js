c7c43345915cfb7bbe4bdcdaeaf6156717a4bf6a
Adam Błaszczyk <adaminko12@gmail.com>
2023-01-08 15:47:17 +0100

    add for-of

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-const titleClickHandler = function(){
-  console.log('Link was clicked!');
+'use strict';
+const titleClickHandler = function(event){
+  console.log(event);
+/* remove class 'active' from all article links */
+/* add class 'active' to the clicked link */
+/* remove class 'active' from all articles */
+/* get 'href' attribute from the clicked link */
+/* find the correct article using the selector ( value of 'href' attribute) */
+/* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
