380b8e95989fa6c4be8c38d6e9ca2aac8485f97d
Adam Błaszczyk <adaminko12@gmail.com>
2023-01-15 21:16:08 +0100

    fix bug with links

--- js/script.js
+++ js/script.js
@@ -29,35 +29,36 @@ const articleSelector = clickedElement.getAttribute('href');
 const targetArticle = document.querySelector(articleSelector);
 /* [DONE] add class 'active' to the correct article */
 targetArticle.classList.add('active');
+}
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 function generateTitleLinks(){
-  /* remove contents of titleList */
+  /* [DONE]remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
-  /* for each article */
+  /* [DONE]for each article */
 const articles = document.querySelectorAll(optArticleSelector);
-  let html =' ';
+  let html ='';
   for(let article of articles){
-    article.addEventListener('click', generateTitleLinks);
-    console.log(article)
-
-  }
-  /* get the article id */
+  
+  /* [DONE] get the article id */
 const articleId = article.getAttribute('id');
-  /* find the title element */
+  /* [DONE] find the title element */
   /* get the title from the title element */
 const articleTitle = article.querySelector(optTitleSelector).innerHTML;
   /* create HTML of the link */
-
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  
   /* insert link into titleList */
+  html = html + linkHTML;
 }
-}
-
+titleList.innerHTML = html;
 const links = document.querySelectorAll('.titles a');
-
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+  }
 }
+
+generateTitleLinks();
